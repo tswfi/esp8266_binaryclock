86b1f1a0aa06e2770e69d1f7023b8a0547e1c53a
Tatu Wikman <tatu.wikman@gmail.com>
2018-01-02 19:40:09 +0200

    ntp test and maybe got the library information defined correctly?

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,66 @@
 #include <Arduino.h>
+
+#include <NTPClient.h>
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+
 #include <Adafruit_NeoPixel.h>
 
 #define PIN D1
 
+const char *ssid = "Vilkas4G";
+const char *password = "verkkokauppa";
+
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "europe.pool.ntp.org", 3600, 60000);
+
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(16, PIN, NEO_GRB + NEO_KHZ400);
 
-void setup() {
+void setup()
+{
+    Serial.begin(115200);
+
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(500);
+        Serial.print(".");
+    }
+
+    timeClient.begin();
+
     strip.begin();
     strip.show();
 }
 
-void loop() {
+void loop()
+{
 
-  for(uint16_t i=1; i<strip.numPixels()-1; i++) {
-    strip.setPixelColor(i-1, strip.Color(0, 32, 0));
-    strip.setPixelColor(i, strip.Color(0, 64, 0));
-    strip.setPixelColor(i+1, strip.Color(0, 255, 0));
-    strip.show();
-    delay(50);
-    strip.clear();
-  }
+    timeClient.update();
 
+    Serial.println(timeClient.getFormattedTime());
 
-  for(uint16_t i=strip.numPixels()-1; i>1; i--) {
-    strip.setPixelColor(i-1, strip.Color(0, 255, 0));
-    strip.setPixelColor(i, strip.Color(0, 64, 0));
-    strip.setPixelColor(i+1, strip.Color(0, 32, 0));
-    strip.show();
-    delay(50);
-    strip.clear();
-  }
+    delay(1000);
 
+/*     for (uint16_t i = 1; i < strip.numPixels() - 1; i++)
+    {
+        strip.setPixelColor(i - 1, strip.Color(0, 32, 0));
+        strip.setPixelColor(i, strip.Color(0, 64, 0));
+        strip.setPixelColor(i + 1, strip.Color(0, 255, 0));
+        strip.show();
+        delay(50);
+        strip.clear();
+    }
 
+    for (uint16_t i = strip.numPixels() - 1; i > 1; i--)
+    {
+        strip.setPixelColor(i - 1, strip.Color(0, 255, 0));
+        strip.setPixelColor(i, strip.Color(0, 64, 0));
+        strip.setPixelColor(i + 1, strip.Color(0, 32, 0));
+        strip.show();
+        delay(50);
+        strip.clear();
+    }
+ */
 }
